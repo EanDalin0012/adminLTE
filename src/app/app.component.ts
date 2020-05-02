c6f8b3ba7cb2109685b34357f1efd04155182f68
EanDalin0012 <endalined@gmail.com>
2020-05-02 14:24:22 +0700

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { LOCAL_STORAGE, LANGUAGE } from './shared/constants/common.const';
+import { Utils } from './shared/utils/utils.static';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adminLTE';
+  constructor(
+    private translate: TranslateService,
+    private router: Router
+  ) {
+    this.setInitialAppLanguage();
+    if (Utils.getSecureStorage(LOCAL_STORAGE.USER_INFO) === null) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  setInitialAppLanguage() {
+    const localLangCode = Utils.getSecureStorage( LOCAL_STORAGE.LANGUAGE_CODE );
+    const i18n = Utils.getSecureStorage( LOCAL_STORAGE.I18N );
+    if ( !i18n || !localLangCode ) {
+      this.translate.setDefaultLang( LANGUAGE.I18N_EN.toString() );
+      this.translate.use( LANGUAGE.I18N_EN.toString() );
+    } else {
+      this.translate.setDefaultLang( i18n );
+      this.translate.use( i18n );
+    }
+  }
 }
