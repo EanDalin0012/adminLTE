3c5a01efb501535d4a1db5e802fa7e5659b75f43
EanDalin0012 <endalined@gmail.com>
2020-05-16 13:49:17 +0700

    modify upload image

--- src/app/register/register5100/register5100.component.ts
+++ src/app/register/register5100/register5100.component.ts
@@ -1,19 +1,17 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ChunkSettings, ClearEvent, FileInfo, FileRestrictions, RemoveEvent, SelectEvent, SuccessEvent, UploadEvent } from '@progress/kendo-angular-upload';
 import { MainCategoryList } from 'src/app/shared/class-tr/classtr-main-category-list';
-import { SubCategoryRequest } from 'src/app/shared/class-tr/classtr-req-sub-category';
 import { ResponseData } from 'src/app/shared/class-tr/classtr-res-data';
 import { MainCategory } from 'src/app/shared/Class/class-main-category';
 import { BTN_ROLES, LOGO_FILE_EXT } from 'src/app/shared/constants/common.const';
 import { ModalService } from 'src/app/shared/services/modal.service';
 import { ServerService } from 'src/app/shared/services/server.service';
 import { environment } from 'src/environments/environment';
+import { ProductRequest } from '../../shared/class-tr/classtr-req-product';
+import { Product } from '../../shared/class/class-product';
 import { SubCategory } from '../../shared/class/class-sub-category';
 import { RequestDataService } from '../../shared/services/get-data.service';
-import { Product } from '../../shared/class/class-product';
-import { ProductRequest } from '../../shared/class-tr/classtr-req-product';
 import { Utils } from '../../shared/utils/utils.static';
 
 @Component({
@@ -69,21 +67,17 @@ export class Register5100Component implements OnInit {
   public imagePreviews: any;
   public uploadRemoveUrl = 'removeUrl';
   userinfo: any;
+
   public uploadRestrictions: FileRestrictions = {
     allowedExtensions: LOGO_FILE_EXT
   };
-  server = environment.bizServer.server;
-  content = environment.bizServer.context;
-  port = environment.bizServer.port;
 
-  totalserver = this.server + ":" + this.port + '/' + this.content;
-  public uploadSaveUrl = this.totalserver + '/upload/companyProfile';
+  public uploadSaveUrl: string;
 
   selectedFile: File;
   message: string;
   userInfo: any;
 
-  url: string;
   api = '/api/file/upload/product';
   public chunkSettings: ChunkSettings = {
     size: 102400
@@ -103,8 +97,8 @@ export class Register5100Component implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.url = environment.bizServer.server + this.api;
-    this.uploadSaveUrl = this.url;
+    this.uploadSaveUrl = environment.bizServer.server + this.api;
+    this.uploadRemoveUrl = environment.bizServer.server + '/api/file/removeUrl';
     this.valuePrimitiveMainCategory = true;
     this.valuePrimitiveSubCategory = true;
     this.translate.get('Home8100').subscribe((res) => {
@@ -230,9 +224,16 @@ export class Register5100Component implements OnInit {
   }
 
   public onRemove(ev: RemoveEvent): void {
+    if ( this.resourceFileInfoId !== undefined) {
+      ev.data = {
+        resourceFileInfoId: this.resourceFileInfoId
+      };
+    }
+
     ev.files.forEach((file: FileInfo) => {
       this.imagePreviews = null;
     });
+
   }
 
   public onClear(ev: ClearEvent): void {
@@ -240,18 +241,13 @@ export class Register5100Component implements OnInit {
   }
 
   successEventHandler(e: SuccessEvent) {
-    if  (e.response.body.header.result === true) {
-      this.resourceFileInfoId = e.response.body.body.id;
-      const url = e.response.body.body.imageURL;
+    const responseData = e.response.body;
+    if (responseData) {
+      if  (responseData.header.result === true) {
+        this.resourceFileInfoId = responseData.body.id;
+        const url = e.response.body.body.imageURL;
+      }
     }
-    // if (e.response.body.header.result) {
-    //   // this.userInfo = [];
-    //   // this.userInfo = Utils.getSecureStorage('USER_INFO');
-    //   // this.userInfo.corporateUserProfileImageURL = e.response.body.body.corporateUserProfileImageURL;
-    //   // this.util.setSecureStorage('USER_INFO', this.userInfo);
-    //   // this.dataService.companyMessage(e.response.body.body.corporateUserProfileImageURL);
-    //   // this.dataService.ImageMessage(e.files[0]);
-    // }
   }
 
   uploadEventHandler(e: UploadEvent) {
@@ -261,10 +257,6 @@ export class Register5100Component implements OnInit {
       fileImageURL: environment.bizServer.server ,
       file : e.files[0].rawFile
     };
-  }
-
-  public completeEventHandler(val) {
-    // this.log(`All files processed`);
   }
 
 }
